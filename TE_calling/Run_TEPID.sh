e1d1efbbb4f1cab7dbaaddd9b21cc11b2854cea0
Roberthorv <robert.horvath@bluewin.ch>
2023-08-23 17:02:15 +0200

    Update Run_TEPID.sh

--- TE_calling/Run_TEPID.sh
+++ TE_calling/Run_TEPID.sh
@@ -1,1 +1,8 @@
+#Run Trimmomnatic
+# for example on the Ko3_1 files:
+trimmomatic PE Ko3_1.fq.gz Ko3_2.fq.gz Ko3_R1_paired.fastq.gz Ko3_R1_unpaired.fastq.gz Ko3_R2_paired.fastq.gz Ko3_R2_unpaired.fastq.gz ILLUMINACLIP:/cluster/work/gdc/people/rhorvath/Scripts/Adapter_Gordon_2017.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+# Run FastQC
+# for example on the Ko3_R1_paired.fastq.gz file:
+fastqc Ko3_R1_paired.fastq.gz
 
