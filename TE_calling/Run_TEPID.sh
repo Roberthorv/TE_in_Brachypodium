0fbce47fdcae366b5335b1dd739b45995d5fcc9a
Roberthorv <robert.horvath@bluewin.ch>
2023-08-23 17:05:10 +0200

    Update Run_TEPID.sh

--- TE_calling/Run_TEPID.sh
+++ TE_calling/Run_TEPID.sh
@@ -6,3 +6,7 @@ trimmomatic PE Ko3_1.fq.gz Ko3_2.fq.gz Ko3_R1_paired.fastq.gz Ko3_R1_unpaired.fa
 # for example on the Ko3_R1_paired.fastq.gz file:
 fastqc Ko3_R1_paired.fastq.gz
 
+# Run TEPID
+# for example on the Ko3 file:
+tepid-map -x /cluster/scratch/rhorvath/reference_genome/bowtie2_ref_index/Bdistachyon_556_v3.0 -y /cluster/scratch/rhorvath/reference_genome/yaha_ref_index/Bdistachyon_556_v3.0.X15_01_65525S -p 8 -s 300 -n Ko3 -1 /cluster/scratch/rhorvath/FastQ_files/Ko3_R1_paired.fastq.gz -2 /cluster/scratch/rhorvath/FastQ_files/Ko3_R2_paired.fastq.gz -z
+
