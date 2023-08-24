3973385d7125ea5df15ec8b583c98d1ed0a8e45c
Roberthorv <robert.horvath@bluewin.ch>
2023-08-24 13:16:10 +0200

    Create Run_SLiM_simulations.sh

---
+++ Simulations/Run_SLiM_simulations.sh
@@ -0,0 +1,8 @@
+# example of a loop to run SLiM with: negative selection coefficient of -0.025, proportion of netral mutation 0% and 80% selfing
+
+for ((i=1; i<=20; i++))
+do
+echo $i
+/media/HDD2/Robert/SLiM/SLiM/build/slim -d run=$i -d "name='SNPs_sel_-0.025_proportion_1'" -d negative=-0.025 -d proportion=1 -d selfing=0.80 B.distachyon_pop_scaling_50_SNPs_for_shushu_selfing_var.slim
+done
+
