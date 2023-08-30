d079cfbec9a0ef3cf32192d9fb064b9f68f94b60
Roberthorv <robert.horvath@bluewin.ch>
2023-08-30 14:10:40 +0200

    Update Run_GEVA.sh

--- Run_GEVA/Run_GEVA.sh
+++ Run_GEVA/Run_GEVA.sh
@@ -1,3 +1,11 @@
-## 
+## Prepare the genetic map called Bdistachyon_genetic_map.txt
+sed -i -e 's/_//g' Bdistachyon_genetic_map.txt
+
+## split genetic map by scaffolds:
+grep "chr1\|Chromosome" Bdistachyon_genetic_map.txt > Bdistachyon_genetic_map_chr1.txt
+grep "chr2\|Chromosome" Bdistachyon_genetic_map.txt > Bdistachyon_genetic_map_chr2.txt
+grep "chr3\|Chromosome" Bdistachyon_genetic_map.txt > Bdistachyon_genetic_map_chr3.txt
+grep "chr4\|Chromosome" Bdistachyon_genetic_map.txt > Bdistachyon_genetic_map_chr4.txt
+grep "chr5\|Chromosome" Bdistachyon_genetic_map.txt > Bdistachyon_genetic_map_chr5.txt
 
 
