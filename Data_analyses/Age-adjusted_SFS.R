de1b964384b02509be1095dae6b96a88f5d16186
Roberthorv <robert.horvath@bluewin.ch>
2023-08-30 16:37:17 +0200

    Update Age-adjusted_SFS.R

--- Data_analyses/Age-adjusted_SFS.R
+++ Data_analyses/Age-adjusted_SFS.R
@@ -1,2 +1,8 @@
-##
+## Make an age adjusted SFS for each clade
+## use a freqeuncy adjustment at the begining to account for frequency call biases 
 
+library(vcfR)
+
+## get Retrotransposon 
+TIP_TAP_in_vcf_info_bed <- read.table("./TIP_TAP_in_vcf_info.bed", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+Retrotransposon_ID <- TIP_TAP_in_vcf_info_bed[TIP_TAP_in_vcf_info_bed[,5] == "Retrotransposon" & !is.na(TIP_TAP_in_vcf_info_bed[,5]), 4]
