3e5bf3fb9a66205a01c7b62c52c7d89ea82c412e
Roberthorv <robert.horvath@bluewin.ch>
2023-09-05 13:30:25 +0200

    Update Age-adjusted_SFS_based_on_gene_distance.R

--- Data_analyses/Age-adjusted_SFS_based_on_gene_distance.R
+++ Data_analyses/Age-adjusted_SFS_based_on_gene_distance.R
@@ -291,6 +291,225 @@ delta_freq_decile_All_boot_freq_matched_B_West_more_than_5kb <- matrix(unlist(sa
   return(delta_freq_decile_All_resample)
 })), nrow = n_boot, ncol = Nbin, byrow = TRUE)
 
+## plotting the results 
+
+my_names_A_East <- rep(NA, 45)
+for (a in 1:45) {
+  my_names_A_East[a] <- paste0(format(my_bonderies_A_East[floor((a-1)/3)+1], scientific = TRUE, digits = 2), " -\n", format(my_bonderies_A_East[floor((a-1)/3)+2], scientific = TRUE, digits = 2))
+}
+my_names_A_Italia <- rep(NA, 45)
+for (a in 1:45) {
+  my_names_A_Italia[a] <- paste0(format(my_bonderies_A_Italia[floor((a-1)/3)+1], scientific = TRUE, digits = 2), " -\n", format(my_bonderies_A_Italia[floor((a-1)/3)+2], scientific = TRUE, digits = 2))
+}
+my_names_B_West <- rep(NA, 45)
+for (a in 1:45) {
+  my_names_B_West[a] <- paste0(format(my_bonderies_B_West[floor((a-1)/3)+1], scientific = TRUE, digits = 2), " -\n", format(my_bonderies_B_West[floor((a-1)/3)+2], scientific = TRUE, digits = 2))
+}
+my_names_B_East <- rep(NA, 45)
+for (a in 1:45) {
+  my_names_B_East[a] <- paste0(format(my_bonderies_B_East[floor((a-1)/3)+1], scientific = TRUE, digits = 2), " -\n", format(my_bonderies_B_East[floor((a-1)/3)+2], scientific = TRUE, digits = 2))
+}
+
+library(scales)
+my_at <- as.vector(rbind(1:Nbin, 1:Nbin, 1:Nbin))
+my_plot_seq <- as.vector(rbind(c(1:Nbin), c((Nbin+1):(2*Nbin)), c((2*Nbin+1):(3*Nbin))))
+
+pdf(file = "Delta_freqeuncy_freq_matched_TEs_vs_SNPs_combined.pdf", width = 11, height = 5)
+layout(matrix(c(rep(1,19),rep(2,18),rep(3,18),rep(4,18),
+                rep(1,19),rep(2,18),rep(3,18),rep(4,18),
+                rep(5,19),rep(6,18),rep(7,18),rep(8,18),
+                rep(5,19),rep(6,18),rep(7,18),rep(8,18),
+                rep(5,19),rep(6,18),rep(7,18),rep(8,18)), nrow = 5, ncol = 18*4+1, byrow = TRUE))
+par(mar=c(1, 4, 1.75, 0))
+
+boxplot(cbind(delta_freq_decile_All_boot_A_East_non_synonymous, delta_freq_decile_All_boot_A_East_high_effect_SNPs, delta_freq_decile_All_boot_freq_matched_A_East)[,my_plot_seq],
+        col = c(alpha("azure3", 0.7), alpha("azure4", 0.7), alpha("#96127d", 0.7)), xlab = "", ylab = "", cex.main = 2, main = "", 
+        border = c("azure3", "azure4", "#96127d"),
+        names = rep(NA, 45), cex.axis = 1.4,
+        at = my_at, outline = FALSE, ylim = c(-0.45, 0.15))
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("Retrotransposons ","Non-synonymous SNPs","High effect SNPs"), 
+       col = c("#96127d", "azure3", "azure4"), bty = "n", pch = 15, cex = 1.1)
+title(ylab = expression(paste(Delta, " frequency")), mgp = c(2.3, 1, 0), cex.lab = 1.8)
+mtext("A", side = 3, at = -1.5, line = -0.5, cex = 1.3)
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6)
+
+par(mar=c(1, 2.5, 1.75, 0.5))
+
+boxplot(cbind(delta_freq_decile_All_boot_A_Italia_non_synonymous, delta_freq_decile_All_boot_A_Italia_high_effect_SNPs, delta_freq_decile_All_boot_freq_matched_A_Italia)[,my_plot_seq],
+        col = c(alpha("azure3", 0.7), alpha("azure4", 0.7), alpha("#21908c", 0.7)), xlab = "", ylab = "", cex.main = 2, main = "", yaxt = "n", 
+        border = c("azure3", "azure4", "#21908c"), 
+        names = rep(NA, 45), 
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("Retrotransposons ","Non-synonymous SNPs","High effect SNPs"), 
+       col = c("#21908c", "azure3", "azure4"), bty = "n", pch = 15, cex = 1.1)
+mtext("B", side = 3, at = -1, line = -0.5, cex = 1.3)
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6)
+
+boxplot(cbind(delta_freq_decile_All_boot_B_West_non_synonymous, delta_freq_decile_All_boot_B_West_high_effect_SNPs, delta_freq_decile_All_boot_freq_matched_B_West)[,my_plot_seq],
+        col = c(alpha("azure3", 0.7), alpha("azure4", 0.7), alpha("#3B528BFF", 0.7)), xlab = "", ylab = "", cex.main = 2, main = "", yaxt = "n",
+        border = c("azure3", "azure4", "#3B528BFF"),
+        names = rep(NA, 45), 
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("Retrotransposons ","Non-synonymous SNPs","High effect SNPs"), 
+       col = c("#3B528BFF", "azure3", "azure4"), bty = "n", pch = 15, cex = 1.1)
+mtext("C", side = 3, at = -1.2, line = -0.5, cex = 1.3)
+mtext("***", side = 3, at = 11, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6)
+
+boxplot(cbind(delta_freq_decile_All_boot_B_East_non_synonymous, delta_freq_decile_All_boot_B_East_high_effect_SNPs, delta_freq_decile_All_boot_freq_matched_B_East)[,my_plot_seq],
+        col = c(alpha("azure3", 0.7), alpha("azure4", 0.7), alpha("#5DC863FF", 0.7)), xlab = "", ylab = "", cex.main = 2, main = "", yaxt = "n",
+        border = c("azure3", "azure4", "#5DC863FF"),
+        names = rep(NA, 45), 
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("Retrotransposons ","Non-synonymous SNPs","High effect SNPs"), 
+       col = c("#5DC863FF", "azure3", "azure4"), bty = "n", pch = 15, cex = 1.1)
+mtext("D", side = 3, at = -1.2, line = -0.5, cex = 1.3)
+mtext("***", side = 3, at = 11, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6)
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6)
+
+par(mar=c(9.5, 4, 1, 0))
+
+boxplot(cbind(delta_freq_decile_All_boot_freq_matched_A_East_in_gene_and_1kB, delta_freq_decile_All_boot_freq_matched_A_East_1kB_and_5kB, delta_freq_decile_All_boot_freq_matched_A_East_more_than_5kb)[,my_plot_seq],
+        col = c(alpha("#96127d", 1), alpha("#96127d", 0.75), alpha("#96127d", 0.5)), xlab = "", ylab = "", cex.main = 2, main = "", 
+        border = c("black", "azure4", "azure3"),
+        names = rep(NA, 45), cex.axis = 1.4,
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+title(xlab = "Age bins", mgp = c(8, 1, 0), cex.lab = 1.75)
+title(ylab = expression(paste(Delta, " frequency")), mgp = c(2.3, 1, 0), cex.lab = 1.8)
+text(x = my_at + 0.55, y = rep(-0.52, 45), my_names_A_East, srt = 75, xpd = TRUE, cex = 1.25, pos = 2)
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("In genes and 1 kb surrounding","Between 1 and 5 kb away from genes","More than 5 kb away from genes"), 
+       fill = c(alpha("#96127d", 1), alpha("#96127d", 0.75), alpha("#96127d", 0.5)), bty = "n", border = c("black", "azure4", "azure3"), cex = 1)
+mtext("***", side = 3, at = 9, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+mtext("***", side = 3, at = 10, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+mtext("***", side = 3, at = 12, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+mtext("***", side = 3, at = 13, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+mtext("***", side = 3, at = 14, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+mtext("***", side = 3, at = 15, line = -1.1, cex = 0.6, col = alpha("#96127d", 1))
+
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6, col = alpha("#96127d", 0.75))
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6, col = alpha("#96127d", 0.75))
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6, col = alpha("#96127d", 0.75))
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6, col = alpha("#96127d", 0.75))
+
+mtext("***", side = 3, at = 13, line = -1.7, cex = 0.6, col = alpha("#96127d", 0.5))
+mtext("***", side = 3, at = 14, line = -1.7, cex = 0.6, col = alpha("#96127d", 0.5))
+mtext("***", side = 3, at = 15, line = -1.7, cex = 0.6, col = alpha("#96127d", 0.5))
+
+par(mar=c(9.5, 2.5, 1, 0.5))
+
+boxplot(cbind(delta_freq_decile_All_boot_freq_matched_A_Italia_in_gene_and_1kB, delta_freq_decile_All_boot_freq_matched_A_Italia_1kB_and_5kB, delta_freq_decile_All_boot_freq_matched_A_Italia_more_than_5kb)[,my_plot_seq],
+        col = c(alpha("#21908c", 1), alpha("#21908c", 0.75), alpha("#21908c", 0.5)), xlab = "", ylab = "", cex.main = 2, main = "",  yaxt = "n",
+        border = c("black", "azure4", "azure3"), 
+        names = rep(NA, 45), 
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+title(xlab = "Age bins", mgp = c(8, 1, 0), cex.lab = 1.75)
+text(x = my_at + 0.55, y = rep(-0.52, 45), my_names_A_Italia, srt = 75, xpd = TRUE, cex = 1.25, pos = 2)
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("In genes and 1 kb surrounding","Between 1 and 5 kb away from genes","More than 5 kb away from genes"), 
+       fill = c(alpha("#21908c", 1), alpha("#21908c", 0.75), alpha("#21908c", 0.5)), bty = "n", border = c("black", "azure4", "azure3"), cex = 1)
+mtext("***", side = 3, at = 2, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+mtext("***", side = 3, at = 9, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+mtext("***", side = 3, at = 12, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+mtext("***", side = 3, at = 13, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+mtext("***", side = 3, at = 14, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+mtext("***", side = 3, at = 15, line = -1.1, cex = 0.6, col = alpha("#21908c", 1))
+
+mtext("***", side = 3, at = 2, line = -1.4, cex = 0.6, col = alpha("#21908c", 0.75))
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6, col = alpha("#21908c", 0.75))
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6, col = alpha("#21908c", 0.75))
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6, col = alpha("#21908c", 0.75))
+
+mtext("***", side = 3, at = 12, line = -1.7, cex = 0.6, col = alpha("#21908c", 0.5))
+mtext("***", side = 3, at = 14, line = -1.7, cex = 0.6, col = alpha("#21908c", 0.5))
+mtext("***", side = 3, at = 15, line = -1.7, cex = 0.6, col = alpha("#21908c", 0.5))
+
+
+boxplot(cbind(delta_freq_decile_All_boot_freq_matched_B_West_in_gene_and_1kB, delta_freq_decile_All_boot_freq_matched_B_West_1kB_and_5kB, delta_freq_decile_All_boot_freq_matched_B_West_more_than_5kb)[,my_plot_seq],
+        col = c(alpha("#3B528BFF", 1), alpha("#3B528BFF", 0.75), alpha("#3B528BFF", 0.5)), xlab = "", ylab = "", cex.main = 2, main = "",  yaxt = "n",
+        border = c("black", "azure4", "azure3"),
+        names = rep(NA, 45),
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+title(xlab = "Age bins", mgp = c(8, 1, 0), cex.lab = 1.75)
+text(x = my_at + 0.55, y = rep(-0.52, 45), my_names_B_West, srt = 75, xpd = TRUE, cex = 1.25, pos = 2)
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("In genes and 1 kb surrounding","Between 1 and 5 kb away from genes","More than 5 kb away from genes"), 
+       fill = c(alpha("#3B528BFF", 1), alpha("#3B528BFF", 0.75), alpha("#3B528BFF", 0.5)), bty = "n", border = c("black", "azure4", "azure3"), cex = 1)
+mtext("***", side = 3, at = 10, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+mtext("***", side = 3, at = 11, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+mtext("***", side = 3, at = 12, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+mtext("***", side = 3, at = 13, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+mtext("***", side = 3, at = 14, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+mtext("***", side = 3, at = 15, line = -1.1, cex = 0.6, col = alpha("#3B528BFF", 1))
+
+mtext("***", side = 3, at = 2, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 10, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 11, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6, col = alpha("#3B528BFF", 0.75))
+
+mtext("***", side = 3, at = 1, line = -1.7, cex = 0.6, col = alpha("#3B528BFF", 0.5))
+mtext("***", side = 3, at = 3, line = -1.7, cex = 0.6, col = alpha("#3B528BFF", 0.5))
+mtext("***", side = 3, at = 14, line = -1.7, cex = 0.6, col = alpha("#3B528BFF", 0.5))
+mtext("***", side = 3, at = 15, line = -1.7, cex = 0.6, col = alpha("#3B528BFF", 0.5))
+
+
+boxplot(cbind(delta_freq_decile_All_boot_freq_matched_B_East_in_gene_and_1kB, delta_freq_decile_All_boot_freq_matched_B_East_1kB_and_5kB, delta_freq_decile_All_boot_freq_matched_B_East_more_than_5kb)[,my_plot_seq],
+        col = c(alpha("#5DC863FF", 1), alpha("#5DC863FF", 0.75), alpha("#5DC863FF", 0.5)), xlab = "", ylab = "", cex.main = 2, main = "",  yaxt = "n",
+        border = c("black", "azure4", "azure3"),
+        names = rep(NA, 45), 
+        at = my_at, outline=FALSE, ylim = c(-0.45, 0.15))
+axis(2, col.axis = "white")
+title(xlab = "Age bins", mgp = c(8, 1, 0), cex.lab = 1.75)
+text(x = my_at + 0.55, y = rep(-0.52, 45), my_names_B_East, srt = 75, xpd = TRUE, cex = 1.25, pos = 2)
+abline(h = 0, lty = 3)
+legend("bottomleft", legend = c("In genes and 1 kb surrounding","Between 1 and 5 kb away from genes","More than 5 kb away from genes"), 
+       fill = c(alpha("#5DC863FF", 1), alpha("#5DC863FF", 0.75), alpha("#5DC863FF", 0.5)), bty = "n", border = c("black", "azure4", "azure3"), cex = 1)
+mtext("***", side = 3, at = 12, line = -1.1, cex = 0.6, col = alpha("#5DC863FF", 1))
+mtext("***", side = 3, at = 13, line = -1.1, cex = 0.6, col = alpha("#5DC863FF", 1))
+mtext("***", side = 3, at = 14, line = -1.1, cex = 0.6, col = alpha("#5DC863FF", 1))
+mtext("***", side = 3, at = 15, line = -1.1, cex = 0.6, col = alpha("#5DC863FF", 1))
+
+mtext("***", side = 3, at = 2, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 9, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 11, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 12, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 13, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 14, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+mtext("***", side = 3, at = 15, line = -1.4, cex = 0.6, col = alpha("#5DC863FF", 0.75))
+
+mtext("***", side = 3, at = 1, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 2, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 9, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 11, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 13, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 14, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+mtext("***", side = 3, at = 15, line = -1.7, cex = 0.6, col = alpha("#5DC863FF", 0.5))
+
+dev.off()
+
 
 
 
